4e36eeadac10113a89a79fadf6d82b24a165b745
Piotr Kulpinski <piotr@kulpinski.pl>
2026-03-05 23:05:14 +0100

    refactor: improve button group classes

--- components/common/button-group.tsx
+++ components/common/button-group.tsx
@@ -8,7 +8,7 @@ const ButtonGroup = ({ className, ...props }: ButtonGroupProps) => {
     <div
       role="group"
       className={cx(
-        "flex items-center [&>*]:rounded-none [&>*]:focus-within:z-10 [&>*:first-child]:rounded-l-md [&>*:last-child]:rounded-r-md [&>*:not(:first-child)]:-ml-px",
+        "flex items-center *:rounded-none *:focus-within:z-10 *:first:rounded-l-md *:last:rounded-r-md *:not-first:-ml-px",
         className,
       )}
       {...props}
